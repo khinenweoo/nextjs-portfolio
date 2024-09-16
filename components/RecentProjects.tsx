e6f3141ccdb32057a1b4830f676e165334b94688
Khine Nwe Oo <devkhinenweoo@gmail.com>
2024-09-16 11:06:58 +0630

    minor change

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -13,14 +13,14 @@ const RecentProjects = () => {
                 {
                     projects.map(({ id, title, des, img, iconLists, link }) => (
                         <div key={id} className='sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]'>
-                            <PinContainer title={title} href={link}>
+                            <PinContainer title={link} href={link}>
                                 <div className='relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh] mb-10'>
                                     <div className='relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]'>
                                         <img src="/bg.png" alt="bg" />
                                     </div>
                                     <img
                                         src={img}
-                                        alt="title"
+                                        alt={title}
                                         className='z-10 absolute bottom-0'
                                     />
                                 </div>
